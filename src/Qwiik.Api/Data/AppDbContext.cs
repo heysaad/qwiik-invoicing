b275fc0852cc87257cb6e150bbd1acd0066c7919
Saad Shaikh <heysaad@gmail.com>
2026-07-06 16:41:02 +0530

    initial setup

---
+++ src/Qwiik.Api/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Qwiik.Api.Data.Models;
+
+namespace Qwiik.Api.Data;
+
+public class AppDbContext : IdentityDbContext<ApplicationUser>
+{
+}
